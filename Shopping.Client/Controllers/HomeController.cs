660374888358b0296716d8c79da28f76fcee8e3b
Taiwo525 <ayothdav52@gmail.com>
2024-08-20 09:31:52 +0100

    product list added

--- Shopping.Client/Controllers/HomeController.cs
+++ Shopping.Client/Controllers/HomeController.cs
@@ -7,15 +7,36 @@ namespace Shopping.Client.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
+            
+		}
 
-        public IActionResult Index()
+        public IActionResult Index(int count)
         {
-            return View();
+            
+            var products = new List<Product>();
+            var random = new Random();
+            var categories = new[] { "Electronics", "Books", "Clothing", "Toys", "Groceries" };
+            count = 100;
+            for (int i = 1; i <= count; i++)
+            {
+                var product = new Product
+                {
+                    Id = i,
+                    Name = $"Product {i}",
+                    Description = $"This is the description for product {i}.",
+                    Category = categories[random.Next(categories.Length)],
+                    Price = (decimal)(random.NextDouble() * 999.99 + 0.01),
+                    ImageFile = $"image{i}.jpg",
+                    CreatedDate = DateTime.UtcNow
+                };
+                products.Add(product);
+            }
+
+           
+            return View(products);
         }
 
         public IActionResult Privacy()
