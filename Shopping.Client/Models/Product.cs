fb9bdd53e264ed26bd6e374ce163c974503269ef
Taiwo525 <ayothdav52@gmail.com>
2024-08-20 08:25:38 +0100

    Add project files.

---
+++ Shopping.Client/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Client.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0.01, 1000000)]
+        public decimal Price { get; set; }
+        public string ImageFile { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
